c804122016bf69e3661ddc2fc4de8a5b44ccf2b4
skhumkane <skhumkane27@yahoo.com>
2015-03-04 16:07:18 +0200

    Fraction class1

--- Tut2/Source.cpp
+++ Tut2/Source.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 class Fraction
 {
+private:
+	int numerator;
+	int denominator;
+
 public:
+	Fraction(int n, int d)
+	{
+		void setNumerator(int n);
+		void setDenominator(int d);
+	}
+	~Fraction();
+
+
+		
 
 }
